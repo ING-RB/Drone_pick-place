d5f1f2ec4c19b8f9c30d56a11e3012d43b7351d1
ING-RB <v.rossibrunori@gmail.com>
2025-10-26 11:47:45 +0100

    tolto il submodule PX4 e aggiornati instuction e setup

--- ros_ws/install/setup.sh
+++ ros_ws/install/setup.sh
@@ -1,22 +1,22 @@
+# PX4
+cd /home/ros_workspace
+git clone https://github.com/PX4/PX4-Autopilot.git --recursive
 cd /home/ros_workspace/PX4-Autopilot
 make px4_sitl
-
 cd /home/ros_workspace/install
 cp ./model.sdf /home/ros_workspace/PX4-Autopilot/Tools/simulation/gz/models/x500_lidar_2d/model.sdf
 cp ./default.sdf /home/ros_workspace/PX4-Autopilot/Tools/simulation/gz/worlds/default.sdf
 cp ./4013_gz_x500_lidar_2d /home/ros_workspace/PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/airframes/4013_gz_x500_lidar_2d
-
 cd /home/ros_workspace/PX4-Autopilot
 make px4_sitl
 
+
+# LI-SLAM
 cd /home/ros_workspace/LI-SLAM
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
 
-cd /home/ros_workspace
-wget https://d176tv9ibo4jno.cloudfront.net/latest/QGroundControl-x86_64.AppImage
-chmod +x QGroundControl-x86_64.AppImage
 
-
+# Micro-XRCE-DDS-Agent
 cd /home/ros_workspace/Micro-XRCE-DDS-Agent
 mkdir build
 cd build
@@ -25,7 +25,26 @@ make
 make install 
 ldconfig /usr/local/lib/ 
 
+
+# Attach_plugin
+cd /home/ros_workspace/Attach_plugin/build
+cmake ..
+make
+
+
+# drone
+cd /home/ros_workspace/drone
+colcon build
+
+
+# MATLAB
 cd /usr/local/MATLAB/R2025a/sys/mwds/glnxa64/packages/glnxa64
 7z x mathworksservicehost.enc.zip
 cd /usr/local/MATLAB/R2025a/bin/glnxa64
 7z x libcef.zip
+
+
+# QGroundControl
+cd /home/ros_workspace
+wget https://d176tv9ibo4jno.cloudfront.net/latest/QGroundControl-x86_64.AppImage
+chmod +x QGroundControl-x86_64.AppImage
